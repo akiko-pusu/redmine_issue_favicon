c3e54d9bdf5daea74dcb461bf3b7dc57e4dc494b
Akiko Takano <akiko.pusu@gmail.com>
2017-08-06 21:03:52 +0900

    Change README from rdoc to md. Change url of repository. (From bitbucket /hg to Github)

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :redmine_issue_favicon do
   author 'Akiko Takano'
   description 'Plugin to show the number of assigned issues on favicon with badge, using favico.js.'
   version '0.0.2'
-  url 'https://bitbucket.org/akiko_pusu/redmine_issue_favicon'
+  url 'https://github.com/akiko-pusu/redmine_issue_favicon'
   author_url 'http://twitter.com/akiko_pusu'
   requires_redmine version_or_higher: '3.0.0'
 
