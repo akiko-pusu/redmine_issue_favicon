a8a28e4a61bdaf4ae51ce7a9ef96a952da05e2d7
Boris Gorbylev <ekho@ekho.name>
2020-03-11 10:18:40 +0300

    Support Rails 5.x

--- db/migrate/20160706000825_remove_enabled_from_issue_favicon_user_setting.rb
+++ db/migrate/20160706000825_remove_enabled_from_issue_favicon_user_setting.rb
@@ -1,4 +1,9 @@
-class RemoveEnabledFromIssueFaviconUserSetting < ActiveRecord::Migration
+if ActiveRecord.gem_version >= Gem::Version.new('5.0')
+  class RemoveEnabledFromIssueFaviconUserSetting < ActiveRecord::Migration[4.2]; end
+else
+  class RemoveEnabledFromIssueFaviconUserSetting < ActiveRecord::Migration; end
+end
+RemoveEnabledFromIssueFaviconUserSetting.class_eval do
   def self.up
     remove_column :issue_favicon_user_settings, :enabled
   end
