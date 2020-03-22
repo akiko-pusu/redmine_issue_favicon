ea31d3c1b4ccb369765fad5508101d2649c0540b
Akiko Takano <akiko.pusu@gmail.com>
2020-03-22 11:43:05 +0900

    Update version to 0.0.4.

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'redmine'
 require 'issue_favicon_application_hooks'
 require 'issue_favicon_my_account_hooks'
@@ -7,16 +9,14 @@ Rails.configuration.to_prepare do
   # Guards against including the module multiple time (like in tests)
   # and registering multiple callbacks
   require_dependency 'user_preference'
-  unless UserPreference.included_modules.include? IssueFaviconUserPreferencePatch
-    UserPreference.send(:include, IssueFaviconUserPreferencePatch)
-  end
+  UserPreference.include IssueFaviconUserPreferencePatch unless UserPreference.included_modules.include? IssueFaviconUserPreferencePatch
 end
 
 Redmine::Plugin.register :redmine_issue_favicon do
   name 'Redmine Issue Favicon plugin'
   author 'Akiko Takano'
   description 'Plugin to show the number of assigned issues on favicon with badge, using favico.js.'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/akiko-pusu/redmine_issue_favicon'
   author_url 'http://twitter.com/akiko_pusu'
   requires_redmine version_or_higher: '3.0.0'
