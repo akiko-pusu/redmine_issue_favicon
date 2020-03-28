0d83a7f285e9dff5b0e12e19ec05b1358e231253
Akiko Takano <akiko.pusu@gmail.com>
2020-03-28 12:48:09 +0900

    Update version to 0.1.0.

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_issue_favicon do
   name 'Redmine Issue Favicon plugin'
   author 'Akiko Takano'
   description 'Plugin to show the number of assigned issues on favicon with badge, using favico.js.'
-  version '0.0.4'
+  version '0.1.0'
   url 'https://github.com/akiko-pusu/redmine_issue_favicon'
   author_url 'http://twitter.com/akiko_pusu'
   requires_redmine version_or_higher: '3.0.0'
